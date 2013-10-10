ec0cb21b50cf54456607d9f91ebe8a1d7db91a45
eli shkurkin <eshkurkin@gmail.com>
2013-10-10 14:36:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveJazzCalendar::Application.config.secret_token = '3173d9b2b2cc7506e3c504921615a510d6a76c93198907e7a59977cf82c6744676d7c0563b757ad9c0405645a59a6569d72cc5a49a3cab579e2cb6050ce78d6f'
