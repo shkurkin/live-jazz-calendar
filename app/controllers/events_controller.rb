04dfbb4169da4b6107da7eeca9764d31ae004cef
eli shkurkin <eshkurkin@gmail.com>
2013-11-02 23:34:33 -0700

    Show event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,16 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(params[:event])
     if @event.save
-
+      redirect_to @event
     else
       render 'index'
     end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
   end
 end
